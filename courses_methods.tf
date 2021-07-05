aea8b60bd2ef6792db70bfd7d8c4cf2419dbffc6
myanees284 <anees2184@gmail.com>
2021-07-06 00:03:06 +0400

    added missing DELETE method in access control allow methods

--- courses_methods.tf
+++ courses_methods.tf
@@ -183,7 +183,7 @@ resource "aws_api_gateway_integration_response" "course_child_integration_respon
   status_code = "200"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,DELETE'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
   response_templates = {
